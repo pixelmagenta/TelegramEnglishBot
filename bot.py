ce0743b6c5efbe2fcc09228549c0b3dc45864146
pixelmagenta <eugustinova@gmail.com>
2018-01-21 23:06:10 +0300

    added group_handler

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from envparse import env
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import Updater
 import logging
 
@@ -23,8 +24,17 @@ def echo(bot, update):
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
+def group(bot, update):
+    reply_keyboard = [['БПМ-171', 'БПМ-172', 'БПМ-173', 'БПМ-174']]
 
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'What is your group?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
 
+group_handler = CommandHandler('group', group)
+dispatcher.add_handler(group_handler)
 
 
 
