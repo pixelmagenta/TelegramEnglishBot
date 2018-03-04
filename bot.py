2f69affe18c4dde18473a1599d3e5b07e01b1e23
pixelmagenta <eugustinova@gmail.com>
2018-03-04 21:33:56 +0300

    great changes update.message->message

--- bot.py
+++ bot.py
@@ -22,13 +22,18 @@ def registered(func):
         try:
             student = Student.get(user_id=update.effective_chat.id)
         except Student.DoesNotExist:
-            update.message.reply_text('You are not registered yet.\n'+
+            message.reply_text('You are not registered yet.\n'+
                                         'Write your invite code, please')
             return State.AUTH
         else:
-            return func(bot, update, student, *args, **kwargs)
+            return func(bot, update, *args, **kwargs, student=student)
     return wrapped
 
+def needs_mesage(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        return func(bot, update, *args, **kwargs, message=update.message or update.callback_query.message)
+    return wrapped
 
 class State(Enum):
     AUTH = auto()
@@ -37,126 +42,132 @@ class State(Enum):
     EX2 = auto()
     EX3 = auto()
 
-def start(bot, update):
-    update.message.reply_text(text="I'll help you improve your English :) \n\
+@needs_mesage
+def start(bot, update, message):
+    message.reply_text(text="I'll help you improve your English :) \n\
         Write your invite code, please")
     return State.AUTH
 
-def auth(bot, update):
-    code = update.message.text
+@needs_mesage
+def auth(bot, update, message):
+    code = message.text
     print(code)
     try:
         student = Student.get(invite_code=code.strip())
         student.user_id = update.effective_chat.id
         student.save()
     except Student.DoesNotExist:
-        update.message.reply_text(text="Invite code is invalid.")
+        message.reply_text(text="Invite code is invalid.")
     else:
-        update.message.reply_text(text=f"Hi, {student.name}")
+        message.reply_text(text=f"Hi, {student.name}")
     return menu(bot, update)
 
 @registered
-def menu(bot, update, student):
+@needs_mesage
+def menu(bot, update, student, message):
     reply_keyboard = [['Ex1', 'Ex2', 'Ex3']]
 
-    update.message.reply_text(
+    message.reply_text(
         'Here you can choose which exercise to complete. ',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
     return State.MENU
 
 @registered
-def menu_action(bot, update, student):
-    if update.message.text == 'Ex1':
+@needs_mesage
+def menu_action(bot, update, student, message):
+    if message.text == 'Ex1':
         task=Task.select().where((Task.available_at <= datetime.now()) & (Task.due_to >= datetime.now()) & (Task.data["type"] == "choose_right")).get()
-        update.message.reply_text(text=task.data["instructions"])
-        update.message.reply_text(text=task.text)
+        message.reply_text(text=task.data["instructions"])
+        message.reply_text(text=task.text)
         student.on_task = task
         student.on_stage = 0
         student.save()
-        return ex1(update, student)
-    if update.message.text == 'Ex2':
+        return ex1(update, student, message)
+    if message.text == 'Ex2':
         task=Task.select().where((Task.available_at <= datetime.now()) & (Task.due_to >= datetime.now()) & (Task.data["type"] == "make_sentence")).get()
-        update.message.reply_text(text=task.data["instructions"])
+        message.reply_text(text=task.data["instructions"])
         student.on_task = task
         student.on_stage = 0
         student.save()
-        return ex2(bot, update, student)
-    if update.message.text == 'Ex3':
+        return ex2(bot, update, student, message)
+    if message.text == 'Ex3':
         task=Task.select().where((Task.available_at <= datetime.now()) & (Task.due_to >= datetime.now()) & (Task.data["type"] == "solve_problem")).get()
-        update.message.reply_text(text=task.data["instructions"])
+        message.reply_text(text=task.data["instructions"])
         student.on_task = task
         student.on_stage = 0
         student.save()
-        return ex3(bot, update, student)
+        return ex3(bot, update, student, message)
 
-def ex1(update, student):
+def ex1(update, student, message):
     task=student.on_task
     block = task.data["blocks"][student.on_stage]
     keyboard = [[InlineKeyboardButton(answer, callback_data=answer) for answer in block["answers"]]]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.effective_message.reply_text(block["text"], reply_markup=reply_markup)
+    message.reply_text(block["text"], reply_markup=reply_markup)
     return State.EX1
 
 @registered
-def ex1_handler(bot, update, student):
+@needs_mesage
+def ex1_handler(bot, update, student, message):
     query = update.callback_data
     print(query)
     task=student.on_task
     block = task.data["blocks"][student.on_stage]
     if query.data in block["correct"]:
-        update.effective_message.reply_text(text="That's right :)")
+        message.reply_text(text="That's right :)")
     else:
-        update.effective_message.reply_text(text="That's not right :(")
+        message.reply_text(text="That's not right :(")
     if student.on_stage < len(task.data["blocks"])-1:
         student.on_stage += 1
         student.save()
-        return ex1(update, student)
+        return ex1(update, student, message)
     student.on_stage = None
     student.on_task = None
     student.save()
-    update.effective_message.reply_text(text="Exercise 1 is finished.")
-    return menu(bot, update)
+    message.reply_text(text="Exercise 1 is finished.")
+    return menu(bot, update, message)
 
-def ex2(bot, update, student):
-    task=student.on_task
-    block = task.data["blocks"][student.on_stage]
-    update.message.reply_text(block["text"])
+def ex2(bot, update, student, message):
+    block = student.on_task.data["blocks"][student.on_stage]
+    message.reply_text(block["text"])
     return State.EX2
 
 @registered
-def ex2_handler(bot, update, student):
-    answer = update.message.text
-    task=student.on_task
+@needs_mesage
+def ex2_handler(bot, update, student, message):
+    answer = message.text
+    task = student.on_task
     block = task.data["blocks"][student.on_stage]
     if answer in block["correct"]:
-        update.message.reply_text(text="That's right :)")
+        message.reply_text(text="That's right :)")
     else:
-        update.message.reply_text(text="That's not right :(")
+        message.reply_text(text="That's not right :(")
     if student.on_stage < len(task.data["blocks"])-1:
         student.on_stage += 1
         student.save()
-        return ex2(bot, update, student)
+        return ex2(bot, update, student, message)
     student.on_stage = None
     student.on_task = None
     student.save()
-    return menu(bot, update)
+    return menu(bot, update, message)
 
-def ex3(bot, update, student):
+def ex3(bot, update, student, message):
     task=student.on_task
-    update.message.reply_text(task.data["text"])
+    message.reply_text(task.data["text"])
     return State.EX3
 
 @registered
-def ex3_handler(bot, update, student):
-    answer = update.message.text
+@needs_mesage
+def ex3_handler(bot, update, student, message):
+    answer = message.text
     task=student.on_task
     if answer == task.data["correct"]:
-        update.message.reply_text(text="That's right :)")
+        message.reply_text(text="That's right :)")
     else:
-        update.message.reply_text(text="That's not right :(")
-    return menu(bot, update)
+        message.reply_text(text="That's not right :(")
+    return menu(bot, update, message)
 
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler('start', start)],
@@ -169,7 +180,7 @@ conv_handler = ConversationHandler(
         State.EX3: [MessageHandler(Filters.text, ex3_handler)]
     },
 
-    fallbacks=[], per_message=True
+    fallbacks=[]
 
 )
 
